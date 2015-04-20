b0dda1a8f22cc8865bf5afc8a70191a47140a8fa
Matt <Matt@10.114.9.230>
2015-04-20 16:09:24 -0500

    updated the provider directory

--- src/ProviderDirectory.java
+++ src/ProviderDirectory.java
@@ -2,48 +2,19 @@
 
 
 public class ProviderDirectory {
-	private String serviceName;
-	private String serviceCode;
-	private String serviceFee;
 	
 	
-	public ProviderDirectory(String serviceProvided){
-		serviceCode = returnCode(serviceProvided);
-		serviceFee = returnFee(serviceCode);
-		System.out.println(returnDisplay());
+	public ProviderDirectory(){
+		returnInfo();
+		
 	}
 	
-	public String returnCode(String service){
-		if (service == "Excercise Session"){
-			return "4519568";
-		}else if (service == "Weight-loss Crying Session"){
-			return "551947";
-		}else if (service == "Massage Session"){
-			return "102865";
-		}else if (service == "Yoga Session"){
-			return "800085";
-		}else if (service == "Diet Consultation"){
-			return "666665";
-		}
-		return "Service not found";
+	public void returnInfo(){
+		System.out.println("Diet Consultation"+", "+ "666665"+", "+ "50.45");
+		System.out.println("Excercise Sesssion"+", "+ "4519568"+", "+ "12.99");
+		System.out.println("Massage Session"+", "+ "102865"+", "+ "21.45");
+		System.out.println("Weight-loss Crying Session"+", "+ "551947"+", "+ "349.99");
+		System.out.println("Yoga Session"+", "+ "800085"+", "+ "54.69");
 	}
 	
-	public String returnFee(String service){
-		if (serviceCode == "4519568"){
-			return "12.99";
-		}else if (serviceCode == "551947"){
-			return "349.99";
-		}else if (serviceCode == "102865"){
-			return "21.45";
-		}else if (serviceCode == "800085"){
-			return "54.69";
-		}else if (serviceCode == "666665"){
-			return "50.45";
-		}
-		return "Service not found";
-	}
-	
-	public String returnDisplay(){
-		return serviceCode + "," + serviceFee;
-	}
 }
