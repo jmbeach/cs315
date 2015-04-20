c654241b744fd2da5c3df940909b0e08088d36da
Matt <Matt@10.114.9.230>
2015-04-20 17:28:29 -0500

    updated provider report

--- src/ProviderReport.java
+++ src/ProviderReport.java
@@ -1,6 +1,40 @@
-
 public class ProviderReport extends Report {
 	public void generateReport(int memberNumber) {
-		throw new UnsupportedOperationException();
+    }
+    
+    // constructor with no parameters
+	public ProviderReport(){
+		super();
 	}
+	
+    //returns the provider number
+    public int getPNumber(){
+        return providerNumber;
+    }
+    
+    //returns service
+    public int getServiceCode(){
+        return serviceProvided;
+    }
+    
+    //return current date and time
+    public String getDateProvided(){
+        return dateOfServices;
+    }
+    
+    // returns the comments
+	public String getComments(){
+		return comments;
+	}
+    
+	// submits the provider report record
+	public void displayRecord(){
+		System.out.println(getDateTime() + "\n" + getDateProvided() + "\n" + getPNumber() + "\n" + "\n" + getServiceCode() + "\n" + getComments() + "\n" );
+	}
+	
+	
 }
+
+
+
+// provider gets report for only their services that are being used. 
