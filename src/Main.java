8cd07ff6c2232d01ba19e1dfd828d2079485e662
dillonwastrack <dillonwastrack@dhcp-196-243.wireless-resnet.ua.edu>
2015-04-20 21:02:26 -0500

    tested the database class in main

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,17 @@ import java.io.Console;
 public class Main {
 
 	public static void main(String[] args) {
-		Console co = System.console();
-		System.out.println("Welcome to ChocAn!\nMenu:\n\t1)");
-		co.readLine();
-//		String choice = System.console().readLine();
-//		System.out.println(choice);
+		Database d = new Database();
+		Provider p = new Provider("Octavius",123456,"345 anus avenue","Tuscaloosa","AL","35404");
+		d.addProvider(p);
+		Member m = new Member("Dillon",356661,"417 Prince Acres","Tuscaloosa","AL","35404");
+		Member o = new Member("Matt",236544,"417 Prince Acres","Tuscaloosa","AL","35404");
+		d.addMember(m);
+		d.addMember(o);
+		Member mem = d.getMember(o.getNumber());
+		System.out.println(mem.getName());
+		System.out.println(d.getProviders());
+		
 	}
 
 }
