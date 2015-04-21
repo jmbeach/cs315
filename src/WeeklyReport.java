7c7a8ade899403e73bdceb67807d506a0f866747
Matt <Matt@10.114.9.230>
2015-04-21 17:38:15 -0500

    completed and tested weekly summary report :)

--- src/WeeklyReport.java
+++ src/WeeklyReport.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
 
-public class WeeklyReport {
 
+public class WeeklyReport extends Database{
+	private double OverallFee;
+	private int totalProviders;
+	//private ArrayList<Provider> person = new ArrayList<Provider>();
+	private ArrayList<Provider> activePeople = new ArrayList<Provider>();
+	
+	public WeeklyReport(){
+		OverallFee = 0;
+		totalProviders = 0;
+		generateReport();
+		//person = super.getProviders();
+	}
+	
+	public void generateReport(){
+		System.out.println(" --- Begin Weekly report ---");
+		getActives();
+		for (int i=0; i<activePeople.size();i++){
+			OverallFee += activePeople.get(i).getMoneyEarned();
+			totalProviders += 1;
+			System.out.println("Provider Name: " + activePeople.get(i).getName());
+			System.out.println("Number of Consultations: " + activePeople.get(i).getServices().size());
+			System.out.println("Total Weekly Fee: " + activePeople.get(i).getMoneyEarned());
+			System.out.println();
+		}
+		
+		
+		System.out.println("Total number of providers who provided services: " + totalProviders);
+		System.out.println("Overall total fee: " + OverallFee);
+	}
+	
+	public void getActives(){
+		for (int i=0; i<providers.size();i++){
+			if (providers.get(i).getServices().size() > 0){
+				activePeople.add(providers.get(i));
+			}else{
+				continue;
+			}
+		}
+	}
 }
