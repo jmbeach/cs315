b58bbd1a548621ce5bf1710fd1970e9598d42163
Matt <Matt@10.114.9.230>
2015-04-20 20:43:46 -0500

    created a database class - currently done

---
+++ src/Database.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+
+public class Database {
+	
+	ArrayList<Member> members = new ArrayList<Member>();
+	ArrayList<Provider> providers = new ArrayList<Provider>();
+	ArrayList<Record> providerBillRecords = new ArrayList<Record>();
+	
+	public Database(){
+		
+	}
+	
+	public void addMember(Member person){
+		members.add(person);
+	}
+	
+	public void removeMember(Member person){
+		members.remove(person);
+	}
+	
+	public void addProvider(Provider person){
+		providers.add(person);
+	}
+	
+	public void removeProvider(Provider person){
+		providers.remove(person);
+	}
+	
+	public void addBillRecord(Record report){
+		providerBillRecords.add(report);
+	}
+	
+	public void removeBillRecord(Record report){
+		providerBillRecords.remove(report);
+	}
+	
+	public ArrayList<Member> getMembers(){
+		return members;
+	}
+	
+	public Member getMember(int memberNumber){
+		for (int i=0; i<members.size();i++){
+			if (members.get(i).getNumber() == memberNumber){
+				return members.get(i);
+			}
+		}
+		return null;
+	}
+	
+	public Provider getProvider(int providerNumber){
+		for (int i=0; i<providers.size();i++){
+			if (providers.get(i).getNumber() == providerNumber){
+				return providers.get(i);
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<Provider> getProviders(){
+		return providers;
+	}
+	
+	public ArrayList<Record> getRecords(){
+		return providerBillRecords;
+	}
+}
