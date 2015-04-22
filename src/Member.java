64e80b94c88f524b8b648180993341d30d431e8a
Matt <Matt@10.114.9.230>
2015-04-22 13:17:05 -0500

    documentation for member and member report classes

--- src/Member.java
+++ src/Member.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 
 public class Member implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String name;
 	private int number;
@@ -23,6 +20,18 @@ public class Member implements Serializable {
 	private float accountFunds;
 	ArrayList<Record> servicesReceived = new ArrayList<Record>();
 
+	/**
+	 * 
+	 * @param name
+	 * @param number
+	 * @param address
+	 * @param city
+	 * @param state
+	 * @param zipCode
+	 * 
+	 * Constructor for the member class
+	 * 
+	 */
 	public Member(String name, int number, String address, String city,
 			String state, String zipCode) {
 		this.name = name;
@@ -36,102 +45,203 @@ public class Member implements Serializable {
 		accountFunds = 0f;
 	}
 
+	/**
+	 * Constructor for the member class with no arguments
+	 */
 	public Member() {
 		// Member constructor with no arguments
 	}
-
+	
+	/**
+	 * 
+	 * @return
+	 * 
+	 * boolean class that checks to see if a member is signed in or not
+	 * if the member is signed in, returns true, else it returns false
+	 */
 	public boolean isSignedIn() {
 		return isSignedIn;
 	}
 
+	/**
+	 * Sign in method that sets the member sign-in status to true
+	 */
 	public void signIn() {
 		isSignedIn = true;
 	}
 
+	/**
+	 * Sign-out method that sets the member sing-in status to false
+	 */
 	public void signOut() {
 		isSignedIn = false;
 	}
 
+	/**
+	 * 
+	 * @param additionalFunding
+	 * 
+	 * addFunds method that adds additional funding to the member's accounts
+	 */
 	public void addFunds(float additionalFunding) {
 		accountFunds += additionalFunding;
 	}
 
+	/**
+	 * 
+	 * @param amountToTake
+	 * 
+	 * subtractFunds method that removes funds from the member's account
+	 */
 	public void subtractFunds(float amountToTake) {
 		accountFunds -= amountToTake;
 	}
 
+	/**
+	 * getAccountFunds method that returns the amount of funds that the member has in their account
+	 * @return
+	 */
 	public float getAccountFunds() {
 		return accountFunds;
 	}
 
+	/**
+	 *setStatusToValid method that sets the member status to valid
+	 */
 	public void setStatusToValid() {
 		memberStatus = "VALID";
 	}
 
+	/**
+	 * setStatusToInvalid method that sets the member status to Invalid
+	 */
 	public void setStatusToInvalid() {
 		memberStatus = "INVALID";
 	}
 
+	/**
+	 * setStatusToSuspended method that sets the member status to Suspended
+	 */
 	public void setStatusToSuspended() {
 		memberStatus = "SUSPENDED";
 	}
-
+	
+	/**
+	 * getStatus method that returns the value of the member's current status
+	 * @return
+	 */
 	public String getStatus() {
 		return memberStatus;
 	}
 
+	/**
+	 * addServiceReceived method adds a record of a service that the member previously received to the member's object.
+	 * @param serv
+	 */
 	public void addServiceReceived(Record serv) {
 		servicesReceived.add(serv);
 	}
-
+	
+	/**
+	 * getServicesReceived method that returns an ArrayList of all of the services that the member has previously recieved
+	 * @return
+	 */
 	public ArrayList<Record> getServicesReceived() {
 		return servicesReceived;
 	}
 
+	/**
+	 * getName class: that returns the name of the member
+	 * @return
+	 */
 	public String getName() { // Returns the User's Name
 		return name;
 	}
 
+	/**
+	 * setName method: sets the name of a user to the passed in parameter
+	 * @param name
+	 */
 	public void setName(String name) { // Sets the User's Name
 		this.name = name;
 	}
 
+	/**
+	 * getNumber method: returns the member's 9 digit member number
+	 * @return
+	 */
 	public int getNumber() { // Returns the user's 9 digit member number
 		return number;
 	}
 
+	/**
+	 * setNumber method: sets the member's 9 digit member number
+	 * @param number
+	 */
 	public void setNumber(int number) { // Sets the user's 9 digit member number
 		this.number = number;
 	}
 
+	/**
+	 * getAddress method: returns the member's address
+	 * @return
+	 */
 	public String getAddress() { // returns the user's address
 		return address;
 	}
 
+	/**
+	 * setAddress method: sets the member's address
+	 * @param address
+	 */
 	public void setAddress(String address) { // sets the user's address
 		this.address = address;
 	}
 
+	/**
+	 * getCity method: returns the member's city (location)
+	 * @return
+	 */
 	public String getCity() { // returns the user's city
 		return city;
 	}
 
+	/**
+	 * setCity method: sets the member's city
+	 * @param city
+	 */
 	public void setCity(String city) { // sets the user's city
 		this.city = city;
 	}
 
+	/**
+	 * getzipCode method: returns the member's zip code
+	 * @return
+	 */
 	public String getzipCode() { // returns the user's zip code
 		return zipCode;
 	}
 
+	/**
+	 * setZip_Code method: sets the member's zip code
+	 * @param zipCode
+	 */
 	public void setZip_Code(String zipCode) { // sets the user's zip code
 		this.zipCode = zipCode;
 	}
 
+	/**
+	 * getState method: returns the member's state (location)
+	 * @return
+	 */
 	public String getState() { // returns the user's state
 		return state;
 	}
 
+	/**
+	 * setState method: sets the member's state (location)
+	 * @param state
+	 */
 	public void setState(String state) { // sets the user's state
 		this.state = state;
 	}
--- src/MemberReport.java
+++ src/MemberReport.java
@@ -9,12 +9,19 @@ public class MemberReport extends Database {
 	private Record currentRecord;
 	
 	    // constructor with no parameters
+	/**
+	 * Constructor for the MemberReportClass: takes in one member object
+	 * @param personality
+	 */
 	public MemberReport(Member personality){
 		super();
 		this.person = personality;
 		generateReport();
 	}
 	
+	/**
+	 * generateReport method: generates the report for the member using all of the methods in the class
+	 */
 	public void generateReport() {
 		System.out.println("Member Name: " + person.getName());
 		System.out.println("Member Number: " + person.getNumber());
@@ -34,16 +41,29 @@ public class MemberReport extends Database {
 		}
     }
 	
+	/**
+	 * getDateTime method: return
+	 * @return
+	 */
 	public String getDateTime(){
 		return currentRecord.getDate() + " " + currentRecord.getTime();
 	}
 	
     //returns the member number
+	/**
+	 * getProviderNumber method: returns the Provider Number from record that the class is currently looking at
+	 * @return
+	 */
     public int getProviderNumber(){
         return currentRecord.getProviderNumber();
 
     }
     
+    /**
+     * getProviderName method: returns the provider name from record that the class is currently looking at
+     * @param number
+     * @return
+     */
     public String getProviderName(int number){
     	for (int i=0; i<Database.providers.size();i++){
 			if (Database.providers.get(i).getNumber() == number){
@@ -54,21 +74,36 @@ public class MemberReport extends Database {
     }
     
     //returns service
+    /**
+     * getServiceCode method: returns the service code from record that the class is currently looking at
+     * @return
+     */
     public int getServiceCode(){
         return currentRecord.getServiceCode();
     }
     
     //return current date and time
+    /**
+     * getDateProvided method: returns the Date a service was provided from record that the class is currently looking at
+     * @return
+     */
     public String getDateProvided(){
         return currentRecord.getDateOfServices();
     }
     
     // returns the comments
+    /**
+     * getComments method: returns the comments from record that the class is currently looking at
+     * @return
+     */
 	public String getComments(){
 		return currentRecord.getComments();
 	}
     
 	// submits the Member report record
+	/**
+	 * displayRecord method: displays all of the details from one specific record, the record that the class is currently looking at
+	 */
 	public void displayRecord(){
 		System.out.println("Date Provided: " + getDateProvided() + "\n" + "Provider Name: " + getProviderName(getProviderNumber()) + "\n" + "Service Name: " + super.getServiceName(getServiceCode()) );
 	}
